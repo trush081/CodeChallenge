072ba6893b2097f83fda0f34d8e72160b107d60e
trush081 <trush081@gmail.com>
2022-10-15 02:41:21 -0500

    remove dockerfile and add test path

--- src/main/java/com/trentrush/codechallenge/controllers/UserController.java
+++ src/main/java/com/trentrush/codechallenge/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.trentrush.codechallenge.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,10 @@ public class UserController {
     @PostMapping(path = "/user", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CountDTO> saveUser(@RequestBody User user) {
         return ResponseEntity.ok(userService.addUser(user));
+    }
+
+    @GetMapping(path = "/test", produces = MediaType.ALL_VALUE)
+    public String test(){
+        return "Hello World";
     }
 }
